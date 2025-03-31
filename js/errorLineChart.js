535b196bdd2dff1d32369e8ebc2679ba2aa14f56
varshineas <andy.varshine@nih.gov>
2025-03-31 12:04:27 -0400

    changes to the scripts

--- js/errorLineChart.js
+++ js/errorLineChart.js
@@ -1,1 +1,52 @@
-// Updated line chart with error bands
+
+const svgLC = d3.select("#errorLineChart");
+const widthLC = +svgLC.attr("width");
+const heightLC = +svgLC.attr("height");
+
+const timepointsLC = d3.range(10);
+const meanLC = timepointsLC.map(t => ({ x: t, y: 20 + 5 * Math.sin(t / 2) + Math.random() * 5 }));
+const errorLC = 3;
+
+const xLC = d3.scaleLinear().domain([0, 9]).range([50, widthLC - 20]);
+const yLC = d3.scaleLinear().domain([0, 40]).range([heightLC - 30, 20]);
+
+const areaLC = d3.area()
+  .x(d => xLC(d.x))
+  .y0(d => yLC(d.y - errorLC))
+  .y1(d => yLC(d.y + errorLC))
+  .curve(d3.curveMonotoneX);
+
+svgLC.append("path")
+  .datum(meanLC)
+  .attr("fill", "#608BC1")
+  .attr("opacity", 0.3)
+  .attr("d", areaLC);
+
+const lineLC = d3.line()
+  .x(d => xLC(d.x))
+  .y(d => yLC(d.y))
+  .curve(d3.curveMonotoneX);
+
+svgLC.append("path")
+  .datum(meanLC)
+  .attr("fill", "none")
+  .attr("stroke", "#133E87")
+  .attr("stroke-width", 2)
+  .attr("d", lineLC);
+
+svgLC.selectAll("circle")
+  .data(meanLC)
+  .join("circle")
+  .attr("cx", d => xLC(d.x))
+  .attr("cy", d => yLC(d.y))
+  .attr("r", 5)
+  .attr("fill", "#133E87")
+  .on("mouseover", function (event, d) {
+    d3.select(this).transition().attr("r", 7);
+  })
+  .on("mouseout", function () {
+    d3.select(this).transition().attr("r", 5);
+  });
+
+svgLC.append("g").attr("transform", `translate(0, ${heightLC - 30})`).call(d3.axisBottom(xLC));
+svgLC.append("g").attr("transform", `translate(50, 0)`).call(d3.axisLeft(yLC));
--- js/heatmap.js
+++ js/heatmap.js
@@ -1,1 +1,38 @@
-// Updated heatmap with gradient and hover
+
+const svgHM = d3.select("#heatmap");
+const widthHM = +svgHM.attr("width");
+const heightHM = +svgHM.attr("height");
+
+const colsHM = 10, rowsHM = 6;
+const dataHM = d3.range(rowsHM * colsHM).map(i => ({
+  col: i % colsHM,
+  row: Math.floor(i / colsHM),
+  value: Math.random()
+}));
+
+const xHM = d3.scaleBand().domain(d3.range(colsHM)).range([60, widthHM - 20]);
+const yHM = d3.scaleBand().domain(d3.range(rowsHM)).range([20, heightHM - 40]);
+const colorHM = d3.scaleSequential(d3.interpolatePlasma).domain([0, 1]);
+
+svgHM.selectAll("rect")
+  .data(dataHM)
+  .join("rect")
+  .attr("x", d => xHM(d.col))
+  .attr("y", d => yHM(d.row))
+  .attr("width", xHM.bandwidth())
+  .attr("height", yHM.bandwidth())
+  .attr("fill", d => colorHM(d.value))
+  .on("mouseover", function (event, d) {
+    d3.select(this).attr("stroke", "#133E87").attr("stroke-width", 2);
+  })
+  .on("mouseout", function () {
+    d3.select(this).attr("stroke", null);
+  });
+
+svgHM.append("g")
+  .attr("transform", `translate(0, ${heightHM - 40})`)
+  .call(d3.axisBottom(xHM).tickFormat(d => `S${d + 1}`));
+
+svgHM.append("g")
+  .attr("transform", `translate(60, 0)`)
+  .call(d3.axisLeft(yHM).tickFormat(d => `Gene ${d + 1}`));
