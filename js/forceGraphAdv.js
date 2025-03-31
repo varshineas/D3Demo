125544b9576e8e7f1d934eaef74626943cfb5077
varshineas <andy.varshine@nih.gov>
2025-03-31 12:19:42 -0400

    more updates to the charts

--- js/forceGraphAdv.js
+++ js/forceGraphAdv.js
@@ -1,1 +1,92 @@
-// Force-Directed Graph with Zoom + Tooltip
+const svgFGA = d3.select("#forceGraphAdv");
+const widthFGA = +svgFGA.attr("width");
+const heightFGA = +svgFGA.attr("height");
+
+const nodesFGA = d3.range(10).map(i => ({ id: "Node " + i }));
+const linksFGA = d3.range(15).map(() => ({
+  source: "Node " + Math.floor(Math.random() * 10),
+  target: "Node " + Math.floor(Math.random() * 10)
+}));
+
+const simulationFGA = d3.forceSimulation(nodesFGA)
+  .force("link", d3.forceLink(linksFGA).id(d => d.id).distance(80))
+  .force("charge", d3.forceManyBody().strength(-300))
+  .force("center", d3.forceCenter(widthFGA / 2, heightFGA / 2));
+
+const linkFGA = svgFGA.append("g")
+  .attr("stroke", "#999")
+  .attr("stroke-opacity", 0.6)
+  .selectAll("line")
+  .data(linksFGA)
+  .join("line")
+  .attr("stroke-width", 1.5);
+
+const nodeFGA = svgFGA.append("g")
+  .attr("stroke", "#fff")
+  .attr("stroke-width", 1.5)
+  .selectAll("circle")
+  .data(nodesFGA)
+  .join("circle")
+  .attr("r", 10)
+  .attr("fill", "#608BC1")
+  .call(d3.drag()
+    .on("start", dragstartedFGA)
+    .on("drag", draggedFGA)
+    .on("end", dragendedFGA))
+  .on("mouseover", function (event, d) {
+    tooltipFGA.style("display", "block").html(d.id);
+  })
+  .on("mousemove", function (event) {
+    tooltipFGA.style("left", event.pageX + 10 + "px").style("top", event.pageY - 20 + "px");
+  })
+  .on("mouseout", function () {
+    tooltipFGA.style("display", "none");
+  });
+
+const labelFGA = svgFGA.append("g")
+  .selectAll("text")
+  .data(nodesFGA)
+  .join("text")
+  .text(d => d.id)
+  .attr("font-size", 12)
+  .attr("dy", -15)
+  .attr("text-anchor", "middle");
+
+simulationFGA.on("tick", () => {
+  linkFGA
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
+  nodeFGA
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y);
+  labelFGA
+    .attr("x", d => d.x)
+    .attr("y", d => d.y);
+});
+
+const tooltipFGA = d3.select("body").append("div")
+  .style("position", "absolute")
+  .style("background", "white")
+  .style("padding", "5px 10px")
+  .style("border", "1px solid #333")
+  .style("border-radius", "5px")
+  .style("display", "none");
+
+function dragstartedFGA(event, d) {
+  if (!event.active) simulationFGA.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function draggedFGA(event, d) {
+  d.fx = event.x;
+  d.fy = event.y;
+}
+
+function dragendedFGA(event, d) {
+  if (!event.active) simulationFGA.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
--- js/realTimeChart.js
+++ js/realTimeChart.js
@@ -1,1 +1,37 @@
-// Real-Time Updating Line Chart logic will go here
+const svgRTC = d3.select("#realTimeChart");
+const widthRTC = +svgRTC.attr("width");
+const heightRTC = +svgRTC.attr("height");
+
+const dataRTC = d3.range(50).map(() => Math.random() * 100);
+const xRTC = d3.scaleLinear().domain([0, 49]).range([40, widthRTC - 20]);
+const yRTC = d3.scaleLinear().domain([0, 100]).range([heightRTC - 30, 20]);
+
+const lineRTC = d3.line()
+  .x((d, i) => xRTC(i))
+  .y(d => yRTC(d))
+  .curve(d3.curveBasis);
+
+svgRTC.append("path")
+  .datum(dataRTC)
+  .attr("fill", "none")
+  .attr("stroke", "#133E87")
+  .attr("stroke-width", 2)
+  .attr("class", "lineRTC");
+
+svgRTC.append("g")
+  .attr("transform", `translate(0, ${heightRTC - 30})`)
+  .call(d3.axisBottom(xRTC).ticks(10));
+
+svgRTC.append("g")
+  .attr("transform", "translate(40,0)")
+  .call(d3.axisLeft(yRTC));
+
+setInterval(() => {
+  dataRTC.push(Math.random() * 100);
+  dataRTC.shift();
+  svgRTC.select(".lineRTC")
+    .datum(dataRTC)
+    .transition()
+    .duration(500)
+    .attr("d", lineRTC);
+}, 1000);
